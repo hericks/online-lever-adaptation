37f94d9b038727687a8fe79b5cabe1b650791a90
Matthias Hericks <matthias.hericks@gmail.com>
2022-07-22 15:14:20 +0100

    Simple example on how to step through environment.

---
+++ 01_step_through_env.py
@@ -0,0 +1,24 @@
+import torch
+
+from levers import IteratedLeverEnvironment
+from levers.partners import RandomPartner, FixedPatternPartner
+
+
+# Environment parameters
+payoffs = [1., 1.]
+n_iterations = 10
+partner = FixedPatternPartner([0, 1, 0, 0])
+
+# Initialize environment
+env = IteratedLeverEnvironment(payoffs, n_iterations, partner)
+
+# Reset environment
+obs = env.reset()
+print(f'obs: {obs} (initial)')
+
+# Step through environment
+done = False
+while not done:
+    action = 0
+    obs, reward, done = env.step(action)
+    print(f'obs: {obs}, reward: {reward}, done: {done}') 
