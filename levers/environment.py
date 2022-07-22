1696f0bd81a0d7fdf98d1959adf853507f762dc9
Matthias Hericks <matthias.hericks@gmail.com>
2022-07-22 14:07:09 +0100

    Added explanatory comments and basic step function.

--- levers/environment.py
+++ levers/environment.py
@@ -1,7 +1,9 @@
-from .partners import LeverPartner
+from typing import List, Tuple
 
 import torch
 from torch import tensor
+
+from .partners import LeverPartner
 
 
 
@@ -10,36 +12,51 @@ class IteratedLeverEnvironment:
 
     def __init__(
         self, 
-        payoffs: torch.Tensor,
+        payoffs: List,
         n_iterations: int, 
         partner: LeverPartner,
         include_step: bool = True,
         include_payoffs: bool = True
     ):
-        # tensor of lever payoffs of shape torch.Size([n_levers])
-        self.payoffs = payoffs
+        # Tensor of lever payoffs of shape torch.Size([n_levers])
+        self.payoffs = torch.tensor(payoffs, dtype=torch.float32)
         self.partner = partner
 
-        # episode length and number number of steps taken in current episode
-        self.n_iterations = n_iterations
+        # Episode length and number number of steps taken in current episode
+        self.episode_length = n_iterations
         self.episode_step = 0
 
-        # defines how the player's oberservations are constructed
+        # Defines how the player's oberservations are constructed
         self.include_step = include_step
         self.include_payoffs = include_payoffs
 
-        # last actions taken
-        self.last_action = None
+        # Last actions taken
+        self.last_player_action = None
         self.last_partner_action = None
 
-    def reset(self):
+    def reset(self) -> torch.Tensor:
         """Reset the environment (including the partner policy). """
         self.episode_step = 0 
         self.partner.reset()
-        self.last_action = None
+        self.last_player_action = None
         self.last_partner_action = None
-        # return (initial) state for player
+        # Return (initial) state for player
         return self._get_obs()
+
+    def step(self, action) -> Tuple[torch.Tensor, float, bool]:
+        """
+        Takes a single environment step based on player's action and returns
+        the a tuple (new_observation: torch.Tensor, reward: float, done: bool).
+        """
+        partner_action = self.partner.act(
+            payoffs=self.payoffs,
+            episode_step=self.episode_step,
+            last_player_action=self.last_player_action,
+            last_partner_action=self.last_partner_action
+        )
+        self.episode_step +=1
+        done = self.episode_step == self.episode_length
+        return (tensor([0, 0, 0]), 1.0, done)  
 
     def _get_obs(self):
         """Return the players observation of the current state. """
@@ -47,7 +64,7 @@ class IteratedLeverEnvironment:
         step = tensor([self.episode_step]) if self.include_step else empty
         payoffs = self.payoffs if self.include_payoffs else empty
         partner_action = torch.tensor([0] * len(self.payoffs))
-        # only filter None (not zero)
+        # Only filter None (not zero)
         if self.last_partner_action is not None:            
             partner_action[self.last_partner_action] = 1
         # [current_step? payoffs? partner-action (1-hot)]
