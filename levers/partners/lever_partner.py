851521d212784de7a7fbbf0a5e30c84c7dd896d3
Matthias Hericks <matthias.hericks@gmail.com>
2022-07-22 14:07:43 +0100

    Updated step function of abstract LeverAgent.

--- levers/partners/lever_partner.py
+++ levers/partners/lever_partner.py
@@ -1,10 +1,22 @@
+import torch
+
+
 class LeverPartner:
 
     def __init__(self):
+        """Abstract __init__() method. """
         pass
 
-    def act():
+    def act(
+        self,
+        payoffs: torch.Tensor, 
+        episode_step: int,
+        last_player_action: int,
+        last_partner_action: int,
+    ):
+        """Abstract act() method. """
         raise NotImplementedError
 
-    def reset():
+    def reset(self):
+        """Abstract reset() method. """
         raise NotImplementedError
