3b542ee798c910c4a4bb710fbc081f24631c9544
Matthias Hericks <matthias.hericks@gmail.com>
2022-07-26 12:07:49 +0100

    Delete experiment skeleton (to be replaced).

--- experiments/00_skeleton.py
+++
@@ -1,6 +0,0 @@
-# Relative imports outside of package
-import sys, os
-sys.path.insert(1, os.path.join(sys.path[0], '..'))
-
-from levers import IteratedLeverEnvironment
-from levers.partners import FixedPatternPartner
