c3e8ccb6b22bda65fb1b2709bab54eb3d748dce4
Matthias Hericks <matthias.hericks@gmail.com>
2022-07-28 22:12:37 +0100

    Extend for usage with DRQN agents.

--- levers/learner/replay_memory.py
+++ levers/learner/replay_memory.py
@@ -1,27 +1,39 @@
+from typing import NamedTuple, Union, List
+from collections import deque
+
 import random
+import torch
 
-from collections import deque, namedtuple
+
+class Transition(NamedTuple):
+    state: torch.Tensor
+    action: int
+    next_state: torch.Tensor
+    reward: float
+    done: bool
 
 
-Transition = namedtuple(
-    'Transition',
-    ('state', 'action', 'next_state', 'reward', 'done')
-)
+class Trajectory(NamedTuple):
+    observations: torch.Tensor
+    actions: torch.Tensor
+    rewards: torch.Tensor
+    dones: torch.Tensor
 
 
 class ReplayMemory():
-    """
-    Replay memory to store state-action-state transitions used for q-learning.
-    """
 
     def __init__(self, capacity: int):
+        """
+        Replay memory to store either state-action-state transitions or 
+        trajectories used for q-learning.
+        """
         self.memory = deque([], maxlen=capacity)
 
-    def push(self, transition: Transition):
+    def push(self, element: Union[Transition, Trajectory]):
         """Pushes transition to the replay memory. """
-        self.memory.append(transition)
+        self.memory.append(element)
 
-    def sample(self, batch_size: int):
+    def sample(self, batch_size: int) -> List[Union[Transition, Trajectory]]:
         """Returns sample (of size batch_size) of replay memory. """
         return random.sample(self.memory, batch_size)
 
