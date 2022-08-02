99e65189815eb548899536d8bfbe9859e002177b
Matthias Hericks <matthias.hericks@gmail.com>
2022-08-02 20:40:38 +0100

    In-progress experiment. Pushed for local development only.

---
+++ experiments/online_qlearner_all_length3/submit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --time=12:00:00
+#SBATCH --output=./logs/log_%j.log
+#SBATCH --job-name=single_core
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=8G
+#SBATCH --partition=short
+
+# Load necessary applications
+module load Anaconda3
+module load CUDA/11.3.1
+module load cuDNN/8.2.1.32-CUDA-11.3.1
+
+# Load conda environment
+source activate $HOME/.conda/envs/torch
+
+python3 -u online_qlearner_length3.py
