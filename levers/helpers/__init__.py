e78893fdfbbe9256dc0aacf57f0b2c7e70b1943e
Matthias Hericks <matthias.hericks@gmail.com>
2022-08-19 13:55:35 +0100

    Add function to count total number of parameters in network.

--- levers/helpers/__init__.py
+++ levers/helpers/__init__.py
@@ -1,2 +1,2 @@
 from .evaluators import train_drqn_agent, eval_drqn_agent, eval_drqn_agents
-from .helpers import generate_binary_patterns
+from .helpers import generate_binary_patterns, n_total_parameters
--- levers/helpers/helpers.py
+++ levers/helpers/helpers.py
@@ -1,5 +1,7 @@
 from typing import List, Tuple
 
+import torch.nn as nn
+
 
 def generate_binary_patterns(length: int) -> List[Tuple[int]]:
     """
@@ -10,3 +12,9 @@ def generate_binary_patterns(length: int) -> List[Tuple[int]]:
     else:
         tails = generate_binary_patterns(length-1)
         return [(0,) + tail for tail in tails] + [(1,) + tail for tail in tails]
+
+def n_total_parameters(net: nn.Module) -> int:
+    """
+    Returns the total number of parameters in network `net`. 
+    """
+    return sum(p.nume() for p in net.parameters())
