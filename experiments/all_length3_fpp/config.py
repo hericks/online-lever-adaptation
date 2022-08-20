c7fdde3833f8ba8353d81fcda5c97e6f51449e5d
Matthias Hericks <matthias.hericks@gmail.com>
2022-08-20 16:21:46 +0100

    Temporarily remove experiments.

--- experiments/all_length3_fpp/config.py
+++
@@ -1,7 +0,0 @@
-# Output settings
-models_path = '../models'
-train_stats_path = '../train_stats'
-figs_path = '../figures'
-
-experiment_name = 'all_length3'
-model_name_template = 'DRQN-{partner_patterns}-{train_id}'
