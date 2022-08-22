2338492854043805a8835563879e34ebadc06937
Matthias Hericks <matthias.hericks@gmail.com>
2022-08-22 18:33:46 +0100

    Split submit by learner's type.

---
+++ experiments/all_length3_fpp/drqn_submit.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH --time=24:00:00
+#SBATCH --output=./logs/drqn_%j.log
+#SBATCH --job-name=drqn
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=8G
+#SBATCH --partition=medium
+#SBATCH --clusters=all
+
+# Load necessary applications
+module load Anaconda3
+module load CUDA/11.3.1
+module load cuDNN/8.2.1.32-CUDA-11.3.1
+
+# Load conda environment
+source activate $HOME/.conda/envs/evotorch
+
+# Deep Recurrent Q-Learner experiments
+# python3 -u drqn.py --seed=0 --train_id_start=0 --n_train_evals=5 --save
+# python3 -u drqn.py --seed=1 --train_id_start=5 --n_train_evals=5 --save
+# python3 -u drqn.py --seed=2 --train_id_start=10 --n_train_evals=5 --save
+# python3 -u drqn.py --seed=3 --train_id_start=15 --n_train_evals=5 --save
+# python3 -u drqn.py --seed=4 --train_id_start=20 --n_train_evals=5 --save
+
+# TODO:
+# 
---
+++ experiments/all_length3_fpp/odql_submit.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH --time=48:00:00
+#SBATCH --output=./logs/odql-48cpus_%j.log
+#SBATCH --job-name=odql
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=48
+#SBATCH --mem-per-cpu=2G
+#SBATCH --partition=medium
+#SBATCH --clusters=all
+
+# Load necessary applications
+module load Anaconda3
+module load CUDA/11.3.1
+module load cuDNN/8.2.1.32-CUDA-11.3.1
+
+# Load conda environment
+source activate $HOME/.conda/envs/evotorch
+
+# Online Deep Q-Learner experiments
+# python3 -u odql.py --seed=0 --train_id_start=0 --n_train_evals=1 --save --log_interval=0
+# python3 -u odql.py --seed=1 --train_id_start=1 --n_train_evals=1 --save --log_interval=0
+# python3 -u odql.py --seed=2 --train_id_start=2 --n_train_evals=1 --save --log_interval=0
+# python3 -u odql.py --seed=3 --train_id_start=3 --n_train_evals=1 --save --log_interval=0
+# python3 -u odql.py --seed=4 --train_id_start=4 --n_train_evals=1 --save --log_interval=0
+
+# TODO: 
+python3 -u odql.py --seed=5 --train_id_start=5 --n_train_evals=5 --save --log_interval=0
