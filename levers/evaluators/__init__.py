faa87fbe21e04e72c488b88b1445d1595142fbb1
Matthias Hericks <matthias.hericks@gmail.com>
2022-08-26 14:57:51 +0100

    Make parameter distribution part of eval_DQNAgent interface.

--- levers/evaluators/__init__.py
+++ levers/evaluators/__init__.py
@@ -1,2 +1,6 @@
 from .drqn_evaluators import eval_DRQNAgent_xplay
-from .dqn_evaluators import eval_DQNAgent
+from .dqn_evaluators import (
+    eval_DQNAgent,
+    distribute_param_vec_no_hist_rep,
+    distribute_param_vec_with_hist_rep,
+)
--- levers/evaluators/dqn_evaluators.py
+++ levers/evaluators/dqn_evaluators.py
@@ -1,12 +1,28 @@
-from typing import Optional, List
+from typing import Callable, Optional, List
 from copy import deepcopy
 
 import torch
 import torch.nn as nn
 from torch.nn.utils import vector_to_parameters
 
+
+from ..helpers import n_total_parameters
 from ..learner import DQNAgent, Transition
 from .. import IteratedLeverEnvironment
+
+
+def distribute_param_vec_with_hist_rep(
+    learner: DQNAgent, hist_rep: nn.RNNBase, param_vec: torch.Tensor
+):
+    n_learner = n_total_parameters(learner.q_net)
+    vector_to_parameters(param_vec[:n_learner], learner.q_net.parameters())
+    vector_to_parameters(param_vec[n_learner:], hist_rep.parameters())
+
+
+def distribute_param_vec_no_hist_rep(
+    learner: DQNAgent, hist_rep: nn.RNNBase, param_vec: torch.Tensor
+):
+    vector_to_parameters(param_vec, learner.q_net.parameters())
 
 
 def eval_DQNAgent(
@@ -17,25 +33,23 @@ def eval_DQNAgent(
     train: bool,
     epsilon: Optional[float],
     param_vec: Optional[torch.Tensor] = None,
+    distribute_param_vec: Optional[
+        Callable[[DQNAgent, Optional[nn.RNNBase], torch.Tensor], None]
+    ] = None,
 ):
     """
     Evaluates the q-learning DQN-Agent `learner` in the environment `env`
     by rolling out `n_episodes` episodes. Cumulative reward serves as measure
     of fitness.
     """
+    # Distribute parameter vector if available
     if param_vec is not None:
-        n_learner_params = sum(p.numel() for p in learner.q_net.parameters())
-
-        # Load learner's state
-        vector_to_parameters(
-            param_vec[:n_learner_params], learner.q_net.parameters()
-        )
-
-        # Load history representation's state
-        if hist_rep != None:
-            vector_to_parameters(
-                param_vec[n_learner_params:], hist_rep.parameters()
+        if distribute_param_vec is None:
+            raise ValueError(
+                "Got parameter vector `param_vec` but no function (`distribute_param_vec`) to distribute it."
             )
+
+        distribute_param_vec(learner, hist_rep, param_vec)
 
     # Save to reload after each environment evaluation
     if train:
