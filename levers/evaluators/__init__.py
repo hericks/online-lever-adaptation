993cffb38b469b9b49fb1a351d65fe928bc12880
Matthias Hericks <matthias.hericks@gmail.com>
2022-08-29 20:43:37 +0100

    Move eval_DQNAgent_xplay().

--- levers/evaluators/__init__.py
+++ levers/evaluators/__init__.py
@@ -1,6 +1,7 @@
 from .drqn_evaluators import eval_DRQNAgent_xplay
 from .dqn_evaluators import (
     eval_DQNAgent,
+    eval_DQNAgent_xplay,
     distribute_param_vec_no_hist_rep,
     distribute_param_vec_with_hist_rep,
 )
--- levers/evaluators/dqn_evaluators.py
+++ levers/evaluators/dqn_evaluators.py
@@ -1,10 +1,12 @@
 from typing import Callable, Optional, List
 from copy import deepcopy
 
+import numpy as np
+
 import torch
 import torch.nn as nn
-from torch.nn.utils import vector_to_parameters
 
+from torch.nn.utils import vector_to_parameters
 
 from ..helpers import n_total_parameters
 from ..learner import DQNAgent, Transition
@@ -61,7 +63,7 @@ def eval_DQNAgent(
         obs = env.reset()
         obs_rep, hidden = hist_rep(obs.unsqueeze(0))
         env_return, env_greedy_return, env_n_greedy_steps = 0, 0, 0
-        for _ in range(env.episode_length - int(bootstrap_last_step)):
+        for step in range(env.episode_length - int(bootstrap_last_step)):
             action, greedy = learner.act(obs_rep.squeeze(0), epsilon=epsilon)
             next_obs, reward, done = env.step(action)
             next_obs_rep, next_hidden = hist_rep(next_obs.unsqueeze(0), hidden)
@@ -124,5 +126,113 @@ def eval_DQNAgent(
         eval_stats["return"] += env_ret
         eval_stats["greedy_return"] += env_greedy_ret
         eval_stats["n_greedy_steps"] += env_n_greedy_steps
+
+    return eval_stats
+
+
+def eval_DQNAgent_xplay(
+    env: IteratedLeverEnvironment,
+    agent1: DQNAgent,
+    agent2: DQNAgent,
+    hist_rep1: Optional[nn.RNNBase],
+    hist_rep2: Optional[nn.RNNBase],
+    bootstrap_last_step: bool,
+    train1: bool,
+    train2: bool,
+    epsilon_schedule1: Optional[Callable],
+    epsilon_schedule2: Optional[Callable],
+):
+    def apply_optional_hist_rep(
+        obs: torch.Tensor,
+        hidden: Optional[torch.Tensor],
+        hist_rep: Optional[nn.RNNBase],
+    ):
+        if hist_rep is None:
+            return obs.unsqueeze(0), None
+        else:
+            return hist_rep(obs.unsqueeze(0), hidden)
+
+    def apply_optional_epsilon_schedule(
+        schedule: Optional[Callable], step: int, episode_length: int
+    ):
+        if schedule is None:
+            return 0
+        else:
+            return schedule(step, episode_length)
+
+    joint_obs = env.reset()
+    agent1.reset()
+    agent2.reset()
+
+    obs_rep1, hidden1 = apply_optional_hist_rep(joint_obs[0], None, hist_rep1)
+    obs_rep2, hidden2 = apply_optional_hist_rep(joint_obs[1], None, hist_rep2)
+
+    n_steps = env.episode_length - int(bootstrap_last_step)
+    eval_stats = {
+        "rewards": np.zeros(n_steps),
+        "action1": np.zeros(n_steps),
+        "action2": np.zeros(n_steps),
+        "greedy1": np.full(n_steps, False),
+        "greedy2": np.full(n_steps, False),
+    }
+    for step in range(n_steps):
+        epsilon1 = apply_optional_epsilon_schedule(
+            epsilon_schedule1,
+            step,
+            env.episode_length - int(bootstrap_last_step),
+        )
+        action1, greedy1 = agent1.act(obs_rep1.squeeze(0), epsilon=epsilon1)
+
+        epsilon2 = apply_optional_epsilon_schedule(
+            epsilon_schedule2,
+            step,
+            env.episode_length - int(bootstrap_last_step),
+        )
+        action2, greedy2 = agent2.act(obs_rep2.squeeze(0), epsilon=epsilon2)
+        next_joint_obs, reward, done = env.step([action1, action2])
+
+        next_obs_rep1, next_hidden1 = apply_optional_hist_rep(
+            next_joint_obs[0], hidden1, hist_rep1
+        )
+        next_obs_rep2, next_hidden2 = apply_optional_hist_rep(
+            next_joint_obs[1], hidden2, hist_rep2
+        )
+
+        # Optionally train agents
+        if train1:
+            agent1.update_memory(
+                Transition(
+                    obs_rep1.squeeze(0).detach(),
+                    action1,
+                    next_obs_rep1.squeeze(0).detach(),
+                    reward,
+                    done,
+                )
+            )
+            agent1.train()
+        if train2:
+            agent2.update_memory(
+                Transition(
+                    obs_rep2.squeeze(0).detach(),
+                    action1,
+                    next_obs_rep2.squeeze(0).detach(),
+                    reward,
+                    done,
+                )
+            )
+            agent2.train()
+
+        # Update stats
+        eval_stats["rewards"][step] = reward
+        eval_stats["action1"][step] = action1
+        eval_stats["action2"][step] = action2
+        eval_stats["greedy1"][step] = greedy1
+        eval_stats["greedy2"][step] = greedy2
+
+        # Prepare next step
+        obs_rep1 = next_obs_rep1
+        obs_rep2 = next_obs_rep2
+        hidden1 = next_hidden1
+        hidden2 = next_hidden2
 
     return eval_stats
