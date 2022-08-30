a7613a58d89d567937b86cae3f2d827cadc6f53c
Matthias Hericks <matthias.hericks@gmail.com>
2022-08-30 19:07:17 +0100

    Use black style-guide.

--- 02_q_learning.py
+++ 02_q_learning.py
@@ -16,9 +16,7 @@ torch.manual_seed(seed)
 
 # Initialize environment
 env = IteratedLeverEnvironment(
-    payoffs=[1., 1.], 
-    n_iterations=2, 
-    partner=FixedPatternPartner([0])
+    payoffs=[1.0, 1.0], n_iterations=2, partner=FixedPatternPartner([0])
 )
 
 # Initialize DQN agent
@@ -26,13 +24,13 @@ learner = DQNAgent(
     q_net=nn.Sequential(
         nn.Linear(len(env.dummy_obs()), 4),
         nn.ReLU(),
-        nn.Linear(4, env.n_actions())
+        nn.Linear(4, env.n_actions()),
     ),
     capacity=16,
     batch_size=8,
     lr=0.005,
     tau=1,
-    len_update_cycle=2*10,
+    len_update_cycle=2 * 10,
 )
 
 for episode in range(200):
@@ -53,11 +51,11 @@ for episode in range(200):
         obs = next_obs
 
     # Evaluate learner every 50 episodes
-    if (episode+1) % 50 == 0:
-        print(f'--- AFTER EPISODE {episode+1}')
+    if (episode + 1) % 50 == 0:
+        print(f"--- AFTER EPISODE {episode+1}")
         observations = [
-            torch.tensor([0., 1., 1., 0., 0.]),
-            torch.tensor([1., 1., 1., 1., 0.]) 
+            torch.tensor([0.0, 1.0, 1.0, 0.0, 0.0]),
+            torch.tensor([1.0, 1.0, 1.0, 1.0, 0.0]),
         ]
         for obs in observations:
-            print(f'obs: {obs}, q-values: {learner.q_net(obs)}')
+            print(f"obs: {obs}, q-values: {learner.q_net(obs)}")
