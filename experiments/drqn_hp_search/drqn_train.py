2419d14784035037f1a570512d6cdf1b8a0d830b
Matthias Hericks <matthias.hericks@gmail.com>
2022-09-02 11:11:48 +0100

    DRQN hyperparameter search setup.

--- experiments/drqn_hp_search/drqn_train.py
+++ experiments/drqn_hp_search/drqn_train.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+from itertools import combinations
 from typing import List
 from os import path
 
@@ -14,103 +16,175 @@ from levers.learner import DRQNAgent, DRQNetwork
 from levers.helpers import generate_binary_patterns
 
 
+def get_train_test_split(patterns):
+    ret = []
+    n_train_elements = len(patterns) - 1
+    for train_patterns in combinations(patterns, n_train_elements):
+        test_pattern = [p for p in patterns if p not in train_patterns][0]
+        ret.append((train_patterns, test_pattern))
+    return ret
+
+
+def get_stats_dict(
+    losses: torch.Tensor,
+    train_returns: torch.Tensor,
+    test_returns: torch.Tensor,
+    train_test_splits: List,
+):
+    stats = {
+        "mean_loss": losses.mean(),
+        "sd_loss": losses.std(),
+        "mean_train_return": train_returns.mean(),
+        "sd_train_return": train_returns.std(),
+        "mean_test_return": test_returns.mean(),
+        "sd_test_return": test_returns.std(),
+    }
+
+    for sid, (train_patterns, test_pattern) in enumerate(train_test_splits):
+        stats[
+            f"mean_loss_split_train={train_patterns}_test={test_pattern}"
+        ] = losses[:, sid].mean()
+        stats[
+            f"sd_loss_split_train={train_patterns}_test={test_pattern}"
+        ] = losses[:, sid].std()
+        stats[
+            f"mean_train_return_split_train={train_patterns}_test={test_pattern}"
+        ] = train_returns[:, sid].mean()
+        stats[
+            f"sd_train_return_split_train={train_patterns}_test={test_pattern}"
+        ] = train_returns[:, sid].std()
+        stats[
+            f"mean_test_return_split_train={train_patterns}_test={test_pattern}"
+        ] = test_returns[:, sid].mean()
+        stats[
+            f"sd_test_return_split_train={train_patterns}_test={test_pattern}"
+        ] = test_returns[:, sid].std()
+
+    return stats
+
+
 def train():
     wandb.init()
     config = wandb.config
-    wandb.run.name = f"drqn-lr={config.learning_rate:5.4f}-tau={config.tau:5.4f}-len_update_cycle{config.len_update_cycle}"
+    wandb.run.name = f"lr={config.learning_rate:5.4f}-tau={config.tau:5.4f}-T={config.len_update_cycle}-cap={config.capacity}-bs={config.batch_size}"
+    # wandb.run.name = wandb.run.id
 
     # (0, 0, 0), (0, 0, 1), (0, 1, 0), (0, 1, 1)
     # (1, 0, 0), (1, 0, 1), (1, 1, 0), (1, 1, 1)
-    train_patterns = ((0, 0, 1), (1, 0, 1), (1, 1, 0), (1, 1, 1))
-    train_envs = [
-        IteratedLeverEnvironment(
-            payoffs=[1.0, 1.0],
-            n_iterations=100 + 1,
-            partner=FixedPatternPartner(pattern),
-            include_step=False,
-            include_payoffs=False,
-        )
-        for pattern in train_patterns
-    ]
+    patterns = [(0, 0, 0), (0, 0, 1), (0, 1, 1), (1, 0, 1), (1, 1, 0)]
+
+    dummy_env = IteratedLeverEnvironment(
+        payoffs=[1.0, 1.0],
+        n_iterations=100 + 1,
+        partner=FixedPatternPartner(patterns[0]),
+        include_step=False,
+        include_payoffs=False,
+    )
+    # Create table with results
+    train_test_splits = get_train_test_split(patterns)
+    n_splits = len(train_test_splits)
 
     learners = [
-        DRQNAgent(
-            q_net=DRQNetwork(
-                rnn=nn.LSTM(
-                    input_size=len(train_envs[0].dummy_obs()),
-                    hidden_size=config.rnn_hidden_dim,
-                    batch_first=True,
-                ),
-                fnn=nn.Sequential(
-                    nn.Linear(
-                        in_features=config.rnn_hidden_dim,
-                        out_features=config.fnn_hidden_dim,
+        [
+            DRQNAgent(
+                q_net=DRQNetwork(
+                    rnn=nn.LSTM(
+                        input_size=len(dummy_env.dummy_obs()),
+                        hidden_size=config.rnn_hidden_dim,
+                        batch_first=True,
                     ),
-                    nn.ReLU(),
-                    nn.Linear(
-                        in_features=config.fnn_hidden_dim,
-                        out_features=train_envs[0].n_actions(),
+                    fnn=nn.Sequential(
+                        nn.Linear(
+                            in_features=config.rnn_hidden_dim,
+                            out_features=config.fnn_hidden_dim,
+                        ),
+                        nn.ReLU(),
+                        nn.Linear(
+                            in_features=config.fnn_hidden_dim,
+                            out_features=dummy_env.n_actions(),
+                        ),
                     ),
                 ),
-            ),
-            capacity=config.capacity,
-            batch_size=config.batch_size,
-            lr=config.learning_rate,
-            tau=config.tau,
-            gamma=config.gamma,
-            len_update_cycle=config.len_update_cycle,
-        )
+                capacity=config.capacity,
+                batch_size=min(config.batch_size, config.capacity),
+                lr=config.learning_rate,
+                tau=config.tau,
+                gamma=config.gamma,
+                len_update_cycle=config.len_update_cycle,
+            )
+            for _ in range(n_splits)
+        ]
         for _ in range(config.n_train_evals)
     ]
 
     epsilon = config.eps_start
     for episode in range(config.n_episodes):
-        losses = torch.zeros(config.n_train_evals)
-        epi_returns = torch.zeros(config.n_train_evals)
-        for train_id in range(config.n_train_evals):
-            # Sample and reset environment
-            env = random.sample(train_envs, 1)[0]
-            obs = env.reset()
 
-            # Reset learner
-            learners[train_id].reset_new_episode(init_obs=obs)
+        # Setup results tensors
+        epi_losses = torch.zeros((config.n_train_evals, n_splits))
+        epi_train_returns = torch.zeros_like(epi_losses)
+        epi_test_returns = torch.zeros_like(epi_losses)
 
-            # Reset episode stats
-            epi_return = 0
+        for split_id, (train_patterns, test_pattern) in enumerate(
+            train_test_splits
+        ):
 
-            # Step through environment
-            for step in range(env.episode_length - 1):
-                action = learners[train_id].act(obs, epsilon)
-                next_obs, reward, done = env.step(action)
-                learners[train_id].update_trajectory_buffer(
-                    action, reward, next_obs, done
+            # Setup training environments
+            train_envs = [
+                IteratedLeverEnvironment(
+                    payoffs=[1.0, 1.0],
+                    n_iterations=100 + 1,
+                    partner=FixedPatternPartner(pattern),
+                    include_step=False,
+                    include_payoffs=False,
                 )
-                obs = next_obs
-                #
-                epi_return += reward
+                for pattern in train_patterns
+            ]
 
-            # Update learner
-            learners[train_id].flush_trajectory_buffer()
-            epsilon = max(epsilon - config.eps_diff, config.eps_min)
-            loss = learners[train_id].train()
+            # Setup test environment
+            test_env = deepcopy(train_envs[0])
+            test_env.partner = FixedPatternPartner(test_pattern)
 
-            # Update stats
-            if loss is not None:
-                losses[train_id] = loss
-            else:
-                losses[train_id] = 3
-            epi_returns[train_id] = epi_return
+            for train_id in range(config.n_train_evals):
 
-        # Prepare stats dict for submission to wand
-        stats = {
-            "train_loss_mean": losses.mean(),
-            "train_loss_sd": losses.std(),
-            "episode_return_mean": epi_returns.mean(),
-            "episode_return_sd": epi_returns.std(),
-        }
-        for train_id in range(config.n_train_evals):
-            stats[f"train_loss_{train_id}"] = losses[train_id]
-            stats[f"episode_return_{train_id}"] = epi_returns[train_id]
+                # Obtain agent
+                learner = learners[train_id][split_id]
+
+                # Train agent on randomly samples training environment
+                env = random.sample(train_envs, 1)[0]
+                obs = env.reset()
+                learner.reset_new_episode(init_obs=obs)
+                for _ in range(env.episode_length - 1):
+                    action = learner.act(obs, epsilon)
+                    next_obs, reward, done = env.step(action)
+                    learner.update_trajectory_buffer(
+                        action, reward, next_obs, done
+                    )
+                    obs = next_obs
+                    epi_train_returns[train_id, split_id] += reward
+
+                # Update learner
+                learner.flush_trajectory_buffer()
+                loss = learner.train()
+                epi_losses[train_id, split_id] = loss if loss is not None else 1
+
+                # Test agent on test environment
+                env = test_env
+                obs = env.reset()
+                learner.reset_new_episode(init_obs=obs)
+                for _ in range(env.episode_length - 1):
+                    action = learner.act(obs)
+                    next_obs, reward, done = env.step(action)
+                    obs = next_obs
+                    epi_test_returns[train_id, split_id] += reward
+
+        # Update epsilon
+        epsilon = max(epsilon - config.eps_diff, config.eps_min)
+
+        # Log current episode's stats
+        stats = get_stats_dict(
+            epi_losses, epi_train_returns, epi_test_returns, train_test_splits
+        )
         wandb.log(stats)
 
 
---
+++ experiments/drqn_hp_search/submit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --time=12:00:00
+#SBATCH --output=./logs/refined-sweep/refined-sweep-%j.log
+#SBATCH --job-name=drqn-search
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2G
+#SBATCH --partition=short
+#SBATCH --open-mode=append
+#SBATCH --clusters=all
+
+# Load necessary applications
+module load Anaconda3
+module load CUDA/11.3.1
+module load cuDNN/8.2.1.32-CUDA-11.3.1
+
+# Load conda environment
+source activate $HOME/.conda/envs/evotorch
+
+# Start sweep
+# wandb sweep drqn_random_sweep.yml
+
+# Start agent
+wandb agent hericks/drqn-hyperparameter-search/d8yftxym
