6090546a5c93141e715bb13bce51bd268ad1b2be
Matthias Hericks <matthias.hericks@gmail.com>
2022-09-08 14:33:57 +0100

    Most recent version.

--- experiments/all_length3_fpp/odql_submit.sh
+++ experiments/all_length3_fpp/odql_submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --time=48:00:00
-#SBATCH --output=./logs/odql-48cpus_%j.log
+#SBATCH --output=./logs/odql-test-48cpus-after-restart_%j.log
 #SBATCH --job-name=odql
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=48
@@ -19,18 +19,8 @@ module load cuDNN/8.2.1.32-CUDA-11.3.1
 source activate $HOME/.conda/envs/evotorch
 
 # Online Deep Q-Learner experiments
-# python3 -u odql.py --seed=0 --train_id_start=0 --n_train_evals=1 --save --log_interval=0
-# python3 -u odql.py --seed=1 --train_id_start=1 --n_train_evals=1 --save --log_interval=0
-# python3 -u odql.py --seed=2 --train_id_start=2 --n_train_evals=1 --save --log_interval=0
-# python3 -u odql.py --seed=3 --train_id_start=3 --n_train_evals=1 --save --log_interval=0
-# python3 -u odql.py --seed=4 --train_id_start=4 --n_train_evals=1 --save --log_interval=0
-# python3 -u odql.py --seed=5 --train_id_start=5 --n_train_evals=5 --save --log_interval=0
-# python3 -u odql.py --seed=6 --train_id_start=10 --n_train_evals=5 --save --log_interval=0
-# python3 -u odql.py --seed=7 --train_id_start=15 --n_train_evals=5 --save --log_interval=0
-# python3 -u odql.py --seed=8 --train_id_start=20 --n_train_evals=5 --save --log_interval=0
-
-# Running.
-#
-
-# TODO.
-#
+# python3 -u odql.py --seed=0 --train_id_start=0 --n_train_evals=5 --save --log_interval=0
+# python3 -u odql.py --seed=1 --train_id_start=5 --n_train_evals=5 --save --log_interval=0
+# python3 -u odql.py --seed=2 --train_id_start=10 --n_train_evals=5 --save --log_interval=0
+# python3 -u odql.py --seed=3 --train_id_start=15 --n_train_evals=5 --save --log_interval=0
+python3 -u odql.py --seed=4 --train_id_start=20 --n_train_evals=5 --save --log_interval=0
